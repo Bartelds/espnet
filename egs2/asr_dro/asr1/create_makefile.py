95026789696c58ea638007e7de3473d1ffe70032
ananjan-nandi-9 <tgk.ananjan@gmail.com>
2024-10-26 18:17:34 -0700

    local

--- egs2/asr_dro/asr1/create_makefile.py
+++ egs2/asr_dro/asr1/create_makefile.py
@@ -1,10 +1,11 @@
 LR = [1e-4]
-BATCHING = {"aleb":"ALEB", "sceb":"SCEB"}
-MODELS = {'mms':'MMS', 'xlsr':'XLSR'}
+BATCHING = {"aleb":"ALEB"}
+MODELS = {'mms':'MMS'}
 # MODELS = {'mms':'MMS'}
+EXP='exp046'
 
-file = open('exp005_m.mk', 'r').read()
-file += '\n\n'
+file = open(f'{EXP}_m.mk', 'r').read()
+# file += '\n\n'
 
 for lr in LR:
     for model in MODELS.keys():
@@ -13,21 +14,21 @@ for lr in LR:
 for lr in LR:
     for batching in BATCHING.keys():
         for model in MODELS.keys():
-            file += f"train-{model}-ctc-{batching}-{float(lr)}:\n\t./run_multi.sh $(COMMON_TRAIN_ARGS) $({MODELS[model]}_LOSS_CTC_{float(lr)}_ARGS) $({BATCHING[batching]}_PARAMS)\n\n"
+            file += f"train_{model}_ctc_{batching}_{float(lr)}:\n\t./run_multi.sh $(COMMON_TRAIN_ARGS) $({MODELS[model]}_LOSS_CTC_{float(lr)}_ARGS) $({BATCHING[batching]}_PARAMS)\n\n"
 
 for lr in LR:
     for batching in BATCHING.keys():
         for model in MODELS.keys():
-            file += f"eval-{model}-ctc-{batching}-{float(lr)}: results/$(EXPERIMENT_ID)/\n\t$(EVAL_CMD)\n\n"
+            file += f"eval_{model}_ctc_{batching}_{float(lr)}: results/$(EXPERIMENT_ID)/\n\t$(EVAL_CMD)\n\n"
 
 file += "eval-all: /\n"
 for lr in LR:
     for batching in BATCHING.keys():
         for model in MODELS.keys():
-            file += f"\tmake eval-{model}-ctc-{batching}-{float(lr)} /\n"
+            file += f"\tmake eval_{model}_ctc_{batching}_{float(lr)} /\n"
 file += "\techo 'All done'\n\n"
 
-with open('exp005_auto.mk', 'w') as f:
+with open(f'{EXP}.mk', 'w') as f:
     f.write(file)
 
 bash_file = "!/bin/bash\n\n"
@@ -39,9 +40,9 @@ for lr in LR:
         for model in MODELS.keys():
             idx += 1
             if idx <= 0:
-                bash_file += f"nlprun -n train-{model}-ctc-{batching}-{lr} -g 1 -a asrdro -o results/exp-005/train-{model}-ctc-{batching}-{lr}.txt --mail-user ananjan -d a6000 -p high 'source ../../../tools/activate_python.sh; make train-{model}-ctc-{batching}-{lr}'\n"
+                bash_file += f"nlprun -n train-{model}-ctc-{batching}-{lr} -g 1 -a asrdro -o results/{EXP}/train-{model}-ctc-{batching}-{lr}.txt --mail-user ananjan -d a6000 -p high 'source ../../../tools/activate_python.sh; make train_{model}_ctc_{batching}_{lr}'\n"
             else:
-                bash_file += f"nlprun -n train-{model}-ctc-{batching}-{lr} -g 1 -a asrdro -o results/exp-005/train-{model}-ctc-{batching}-{lr}.txt --mail-user ananjan -d a6000 'source ../../../tools/activate_python.sh; make train-{model}-ctc-{batching}-{lr}'\n"
+                bash_file += f"nlprun -n train-{model}-ctc-{batching}-{lr} -g 1 -a asrdro -o results/{EXP}/train-{model}-ctc-{batching}-{lr}.txt --mail-user ananjan -d a6000 'source ../../../tools/activate_python.sh; make train_{model}_ctc_{batching}_{lr}'\n"
 
 with open('commands_auto.sh', 'w') as f:
     f.write(bash_file)
